6786046105b6b593698efe51ab116a06fccbf0f0
Wang YuLong <ylwang1122@163.com>
2019-05-27 10:44:05 +0800

    Update type.go

--- ping/type.go
+++ ping/type.go
@@ -1,16 +1,3 @@
-// Copyright 2019 JD Inc. All Rights Reserved.
-// type.go - file brief introduce
-/*
-modification history
-----------------------------------------------
-2019/5/26 0:21, by wangyulong3@jd.com, create
-
-*/
-/*
-Description
-
-*/
-
 package ping
 
 import "time"
